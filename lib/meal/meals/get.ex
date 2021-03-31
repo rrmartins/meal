58839991258b7300c9458659a7003ec71137f458
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-31 23:20:14 +0100

    new get module

---
+++ lib/meal/meals/get.ex
@@ -0,0 +1,10 @@
+defmodule Meal.Meals.Get do
+  alias Meal.{Error, Meal, Repo}
+
+  def by_id(id) do
+    case Repo.get(Meal, id) do
+      nil -> {:error, Error.build_meal_not_found_error()}
+      meal -> {:ok, meal}
+    end
+  end
+end
