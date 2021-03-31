32010e54f461b866258f00f4d37caaa8b842a1f8
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-31 19:54:18 +0100

    add create module

---
+++ lib/meal/meals/create.ex
@@ -0,0 +1,16 @@
+defmodule Meal.Meals.Create do
+  alias Meal.{Error, Meal, Repo}
+
+  def call(params) do
+    params
+    |> Meal.changeset()
+    |> Repo.insert()
+    |> handle_insert
+  end
+
+  defp handle_insert({:ok, %Meal{} = result}), do: {:ok, result}
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
+  end
+end
