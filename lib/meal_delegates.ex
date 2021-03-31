e924d1eb8a6138a889b53a8b7fae2c51d1c6636f
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-31 23:30:44 +0100

    add new delegate

--- lib/meal_delegates.ex
+++ lib/meal_delegates.ex
@@ -1,7 +1,10 @@
 defmodule MealDelegates do
+
   alias Meal.Meals.Create, as: MealCreate
+  alias Meal.Meals.Get, as: MealGet
   alias Meal.Meals.Update, as: MealUpdate
 
   defdelegate create_meal(params), to: MealCreate, as: :call
   defdelegate update_meal(params), to: MealUpdate, as: :call
+  defdelegate get_meal_by_id(id), to: MealGet, as: :by_id
 end
