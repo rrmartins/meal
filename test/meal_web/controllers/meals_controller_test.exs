f0f5abd7df737167b1a4cdc4e2e041638d8db3ac
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-31 23:16:34 +0100

    add test from update action

--- test/meal_web/controllers/meals_controller_test.exs
+++ test/meal_web/controllers/meals_controller_test.exs
@@ -41,4 +41,64 @@ defmodule MealWeb.MealsControllerTest do
       assert expected_response == response
     end
   end
+
+  describe "update/2" do
+    test "when there is a meal with the given id, updates the meal", %{conn: conn} do
+      id = "18ad3c36-3af6-47ed-9c02-0821af330673"
+
+      insert(:meal, id: id)
+
+      params = %{
+        "description" => "Abacate"
+      }
+
+      response =
+        conn
+        |> put(Routes.meals_path(conn, :update, id, params))
+        |> json_response(:ok)
+
+      expected_response = %{
+        "meal" => %{
+          "calories" => "300",
+          "publication_date" => "2021-03-28T13:59:13Z",
+          "description" => "Abacate",
+          "id" => "18ad3c36-3af6-47ed-9c02-0821af330673"
+        }
+      }
+
+      assert expected_response == response
+    end
+
+    test "when there are invalid params, returns an error", %{conn: conn} do
+      id = "5d80d5e6-84ae-45b5-8d48-937e4406ff73"
+
+      insert(:meal, id: id)
+
+      params = %{
+        "description" => ""
+      }
+
+      response =
+        conn
+        |> put(Routes.meals_path(conn, :update, id, params))
+        |> json_response(:bad_request)
+
+      expected_response = %{"message" => %{"description" => ["can't be blank"]}}
+
+      assert expected_response == response
+    end
+
+    test "when there isn\'t a meal with the given id, returns an error", %{conn: conn} do
+      id = "7dba804f-d669-488a-95a0-a9c067548ed2"
+
+      response =
+        conn
+        |> put(Routes.meals_path(conn, :update, id))
+        |> json_response(:not_found)
+
+      expected_response = %{"message" => "meal not found"}
+
+      assert expected_response == response
+    end
+  end
 end
