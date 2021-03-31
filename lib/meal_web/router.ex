51267c742cf1f5523033a4a34f74bb8668dcb069
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-31 22:52:54 +0100

    new resource

--- lib/meal_web/router.ex
+++ lib/meal_web/router.ex
@@ -7,6 +7,8 @@ defmodule MealWeb.Router do
 
   scope "/api", MealWeb do
     pipe_through :api
+
+    resources "/meals", MealsController, except: [:new, :edit]
   end
 
   # Enables LiveDashboard only for development
