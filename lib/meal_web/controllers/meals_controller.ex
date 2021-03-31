a2449619294627281b677e600101d05691cbbad9
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-31 23:16:34 +0100

    add update action

--- lib/meal_web/controllers/meals_controller.ex
+++ lib/meal_web/controllers/meals_controller.ex
@@ -13,4 +13,12 @@ defmodule MealWeb.MealsController do
       |> render("create.json", meal: meal)
     end
   end
+
+  def update(conn, params) do
+    with {:ok, %Meal{} = meal} <- MealDelegates.update_meal(params) do
+      conn
+      |> put_status(:ok)
+      |> render("meal.json", meal: meal)
+    end
+  end
 end
