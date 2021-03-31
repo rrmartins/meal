0825bbd9c57d53b8394a023cd382ca6e652fa5a2
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-31 22:52:54 +0100

    change config in mix

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule Meal.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
-      {:ex_machina, "~> 2.7.0", only: :test}
+      {:ex_machina, "~> 2.7.0"}
     ]
   end
 
